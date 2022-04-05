7b48ccc708b73baa21ced218e11635e2b3d40860
fodare <folorunsodamilare007@hotmail.com>
2022-04-05 12:26:41 +0200

    Complete About and Projects page short message

--- src/views/About.jsx
+++ src/views/About.jsx
@@ -1,7 +1,27 @@
 import React from "react";
+import { Greetings } from "../helpers/date";
 
 function About() {
-   return <div className="container">This is the About page</div>;
+   return (
+      <div className="container about">
+         <div className="aboutIntro">
+            <h3>Projects</h3>
+            <p>
+               IT Operations Manager, Systems Engineer and a DevOps enthusiast.
+            </p>
+         </div>
+
+         <div className="aboutMessage">
+            <p>
+               <Greetings /> I'm Däre. I'm an IT Operations manager based in
+               Münster Germany. Welcome to my corner of the internet for
+               solutions on technical related how to's, projects I've created
+               and worked on, solutions to everyday technical problems at work
+               and other things I'd like to share with the universe.
+            </p>
+         </div>
+      </div>
+   );
 }
 
 export default About;
--- src/views/Projects.jsx
+++ src/views/Projects.jsx
@@ -1,7 +1,17 @@
 import React from "react";
 
 function Projects() {
-   return <div className="container">This is the projects page</div>;
+   return (
+      <div className="container">
+         <div className="projectsInto">
+            <h3>Projects</h3>
+            <p>
+               Below are some open-source projects, personal projects I have
+               created on working on.
+            </p>
+         </div>
+      </div>
+   );
 }
 
 export default Projects;
