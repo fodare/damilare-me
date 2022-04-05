66a37dbbfa9f8a005cbc7b8faf39986fcf57e446
fodare <folorunsodamilare007@hotmail.com>
2022-04-05 11:42:45 +0200

    Complete blogs landing page

--- src/views/Blogs.jsx
+++ src/views/Blogs.jsx
@@ -1,7 +1,17 @@
 import React from "react";
 
 function Blog() {
-   return <div className="container">This is the Blogs page</div>;
+   return (
+      <div className="container">
+         <div className="articlesIntro">
+            <h3>Articles</h3>
+            <p>
+               Below you can brwose, techical articles, snippets, development
+               related materials i have collected over time.
+            </p>
+         </div>
+      </div>
+   );
 }
 
 export default Blog;
