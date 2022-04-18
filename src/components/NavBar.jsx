e4e13a0c2570c948f9ef0380492c44b57e077c20
fodare <folorunsodamilare007@hotmail.com>
2022-04-18 14:11:16 +0200

    Add icons to the Navbar links

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -14,16 +14,19 @@ function NavBar() {
             <ul className="nav col-12 col-md-auto mb-2 justify-content-center mb-md-0">
                <li>
                   <a href="/blogs" className="nav-link px-2 link-secondary">
+                     <img src="https://img.icons8.com/external-itim2101-blue-itim2101/25/000000/external-blog-copywriting-itim2101-blue-itim2101-1.png" />{" "}
                      Blogs
                   </a>
                </li>
                <li>
                   <a href="/projects" className="nav-link px-2 link-secondary">
+                     <img src="https://img.icons8.com/ios/25/000000/view-quilt.png" />{" "}
                      Projects
                   </a>
                </li>
                <li>
                   <a href="/about" className="nav-link px-2 link-secondary">
+                     <img src="https://img.icons8.com/external-others-pike-picture/30/000000/external-about-goal-target-purpose-others-pike-picture-2.png" />{" "}
                      About
                   </a>
                </li>
