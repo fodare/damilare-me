f958336783d2239e0bc0f1536c7e1a70ac6c6ecf
fodare <folorunsodamilare007@hotmail.com>
2022-04-21 21:12:46 +0200

    Add gh-pages deployment instructions

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "../components/NavBar";
 import Home from "../views/Home";
 import About from "../views/About";
