156c27414365e1acdd3cca8eb5e4d116b724b822
fodare <folorunsodamilare007@hotmail.com>
2022-04-23 18:09:12 +0200

    Add 404 description and redirect button to homepage

--- src/views/PageNotFound.jsx
+++ src/views/PageNotFound.jsx
@@ -1,7 +1,18 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 function PageNotFound() {
-   return <div className="container">Page not found</div>;
+   return (
+      <div className="container notfound">
+         <h1>404</h1>
+         <p>Page you are looking for does not exist</p>
+         <Link to={"/"}>
+            <button type="button" class="btn notfoundbtn btn-secondary btn-lg">
+               Homepage
+            </button>
+         </Link>
+      </div>
+   );
 }
 
 export default PageNotFound;
