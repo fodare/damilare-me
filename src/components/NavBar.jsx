6dfcd387daec9b882ef41d5a77c1294c85a2e84d
fodare <folorunsodamilare007@hotmail.com>
2022-09-23 14:23:46 +0200

    Change nav logo style

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -4,14 +4,16 @@ function NavBar() {
    return (
       <div className="header-div">
          <header className="d-flex flex-wrap align-items-center container justify-content-center justify-content-md-between py-3 mb-4 ">
+            {/* Logo */}
             <a
                href="/"
-               className="d-flex align-items-center col-md-3 mb-2 mb-md-0 text-dark text-decoration-none"
+               className="d-flex align-items-center col-md-3 mb-2 mb-md-0 text-dark text-decoration-none logo"
             >
-               <h4>Damilare F.</h4>
+               <h2 className="logo">Damilare F.</h2>
             </a>
 
-            <ul className="nav col-12 col-md-auto mb-2 navigation-bar justify-content-center mb-md-0">
+            <ul className="nav col-12 col-md-auto mb-6 navigation-bar justify-content-center mb-md-0">
+               {/* Blogs */}
                <li>
                   <a href="/blogs" className="nav-link px-2 link-secondary">
                      <img
@@ -21,6 +23,8 @@ function NavBar() {
                      Blogs
                   </a>
                </li>
+
+               {/* Projects */}
                <li>
                   <a href="/projects" className="nav-link px-2 link-secondary">
                      <img
@@ -30,6 +34,8 @@ function NavBar() {
                      Projects
                   </a>
                </li>
+
+               {/* Abouts */}
                <li>
                   <a href="/about" className="nav-link px-2 link-secondary">
                      <img
@@ -41,6 +47,7 @@ function NavBar() {
                </li>
             </ul>
 
+            {/* Github Hyperlink */}
             <div className="col-md-3 text-end">
                <a
                   className="me-2"
