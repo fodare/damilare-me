c4f11d23afec7e2b8c3d39996d133da2077aa14b
fodare <folorunsodamilare007@hotmail.com>
2022-11-06 12:31:12 +0100

    Remove education, skills, experience from about page and style miscellaneous link on acout page

--- src/views/About.jsx
+++ src/views/About.jsx
@@ -49,82 +49,14 @@ function About() {
                </span>
             </div>
 
-            <div className="experience">
-               <span>
-                  <h4>
-                     Experience <hr />
-                  </h4>
-               </span>
-
-               <div className="experice-div">
-                  <div className="experience-heading">
-                     <h3>IT Operations Manager</h3>
-                     <p>DEC 2020 - Present</p>
-                  </div>
-
-                  <span className="company-name">
-                     <p>BFS Finance Münster.</p>
-                  </span>
-               </div>
-
-               <div className="experice-div">
-                  <div className="experience-heading">
-                     <h3>Support Service Owner</h3>
-                     <p>JAN 2020 - NOV 2020</p>
-                  </div>
-
-                  <span className="company-name">
-                     <p>AFS IT Services Estonia OÜ.</p>
-                  </span>
-               </div>
-
-               <div className="experice-div">
-                  <div className="experience-heading">
-                     <h3>Technical Support Analyst</h3>
-                     <p>FEB 2019 - DEC 2019</p>
-                  </div>
-
-                  <span className="company-name">
-                     <p>AFS IT Services Estonia OÜ.</p>
-                  </span>
-               </div>
-            </div>
-
-            <div className="skills">
-               <h4>
-                  Skills <hr />
-               </h4>
-               <span>
-                  <li>Languages: Javascript, Java, Python, C#.</li>
-                  <li>
-                     Concepts: DB administration(SQL Lite, MongoDB, SQL), API
-                     Testing & creation, Systems administration, Web3, Data
-                     processing, Risk analysis, Virtualization, Dev OPS,
-                     Cloudification, Web development.
-                  </li>
-               </span>
-            </div>
-
-            <div className="education">
-               <h4>
-                  Education <hr />
-               </h4>
-               <div className="experience-heading">
-                  <h5>Tallinn University of Technology (MSc)</h5>
-                  <p>SEP 2019 - JUN 2020</p>
-               </div>
-               <div className="experience-heading">
-                  <h5>Bangalore University(BSc)</h5>
-                  <p>SEP 2014 - JUN 2017</p>
-               </div>
-            </div>
-
             <div className="miscellaneous">
                <h4>
                   Miscellaneous <hr />
                </h4>
                <li>
-                  <a href="/about/resume">Resume</a>
+                  <a className="miscellaneous-link" href="/about/resume">
+                     Resume
+                  </a>
                </li>
             </div>
          </div>
