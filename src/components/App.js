f83e0e3bd1c824f242433f5042f9c073ad2b3def
fodare <folorunsodamilare007@hotmail.com>
2022-11-06 09:14:03 +0100

    Add path to Resume view

--- src/components/App.js
+++ src/components/App.js
@@ -7,6 +7,7 @@ import Projects from "../views/Projects";
 import Blog from "../views/Blogs";
 import PageNotFound from "../views/PageNotFound";
 import BlogPost from "../views/BlogPost";
+import Resume from "../views/Resume";
 // import Footer from "./Footer";
 
 function App() {
@@ -19,6 +20,7 @@ function App() {
             <Route path="/about" element={<About />} />
             <Route path="/projects" element={<Projects />} />
             <Route path="/blogpost/:query" element={<BlogPost />} />
+            <Route path="/resume" element={<Resume />} />
             <Route path="*" element={<PageNotFound />} />
          </Routes>
       </Router>
