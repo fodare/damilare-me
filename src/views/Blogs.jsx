88351fc2e17224e4204dc6133143d32c7374a6e5
fodare <folorunsodamilare007@hotmail.com>
2023-01-07 21:53:24 +0100

    Removed blog short text intro

--- src/views/Blogs.jsx
+++ src/views/Blogs.jsx
@@ -3,13 +3,9 @@ import PostList from "../components/Postlist";
 
 function Blog() {
    return (
-      <div className="articlediv">
+      <div className="articlediv container">
          <div className="articlesIntro">
             <h3 className="Ariclehead">Articles</h3>
-            <p>
-               Below you can brwose, techical articles, snippets, development
-               related materials i have collected over time.
-            </p>
          </div>
          <div className="postdiv container">
             <PostList />
