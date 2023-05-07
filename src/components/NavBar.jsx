7dc22e61d6c61b47f5fddcb06d33c80e221ae325
fodare <folorunsodamilare007@hotmail.com>
2023-05-07 21:29:27 +0200

    Restyling nav bar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -2,74 +2,62 @@ import React from "react";
 
 function NavBar() {
    return (
-      <div className="header-div">
-         <header className="d-flex flex-wrap align-items-center container justify-content-center justify-content-md-between py-3 mb-4 ">
+      <section className="navigation">
+         <div className="container nav-element">
             {/* Logo */}
-            <a
-               href="/"
-               className="d-flex align-items-center col-md-3 mb-2 mb-md-0 text-dark text-decoration-none logo"
-            >
-               <h2 className="logo">Damilare F.</h2>
-            </a>
-
-            <ul className="nav col-12 col-md-auto mb-6 navigation-bar justify-content-center mb-md-0">
-               {/* Blogs */}
-               <li>
-                  <a
-                     href="/blogs"
-                     className="nav-link px-4 link-secondary blog-link"
-                  >
-                     Blogs
-                  </a>
-               </li>
-
-               {/* Projects */}
-               <li>
-                  <a
-                     href="/projects"
-                     className="nav-link px-4 link-secondary project-link"
-                  >
-                     Projects
-                  </a>
-               </li>
-
-               {/* Tools */}
-               <li>
-                  <a
-                     href="/tools"
-                     className="nav-link px-4 link-secondary project-link"
-                  >
-                     Tools
-                  </a>
-               </li>
-
-               {/* Abouts */}
-               <li>
-                  <a
-                     href="/about"
-                     className="nav-link px-4 link-secondary about-link"
-                  >
-                     About
-                  </a>
-               </li>
-            </ul>
-
-            {/* Github Hyperlink */}
-            <div className="col-md-3 text-end">
-               <a
-                  className="me-2"
-                  href="https://github.com/fodare"
-                  target="_blank"
-                  rel="noopener noreferrer"
-               >
-                  <img
-                     src="https://img.icons8.com/dusk/30/000000/github.png"
-                     alt="Github logo"
-                  />
+            <div className="nav-wrapper">
+               <a href="/" className="item brand">
+                  <span>Damilare F.</span>
                </a>
             </div>
-         </header>
-      </div>
+
+            <div className="nav-section">
+               {/* Nav links */}
+               <div className="nav-routes">
+                  <nav>
+                     <div className="routes">
+                        {/* Blogs */}
+                        <div className="nav-item-outer">
+                           <a href="/" className="route">
+                              <span>Blogs</span>
+                           </a>
+                        </div>
+
+                        {/* Projects */}
+                        <div className="nav-item-outer" k>
+                           <a href="/" className="">
+                              <span>Projects</span>
+                           </a>
+                        </div>
+
+                        {/* Tools */}
+                        <div className="nav-item-outer" k>
+                           <a href="/" className="">
+                              <span>Tools</span>
+                           </a>
+                        </div>
+
+                        {/* About */}
+                        <div className="nav-item-outer" k>
+                           <a href="/" className="">
+                              <span>About</span>
+                           </a>
+                        </div>
+                     </div>
+                  </nav>
+               </div>
+
+               {/* padding div */}
+               <div>
+                  <div className="nav-item-outer ">
+                     <a href="/" className="padding-div">
+                        <span></span>
+                     </a>
+                  </div>
+               </div>
+            </div>
+         </div>
+      </section>
    );
 }
 
