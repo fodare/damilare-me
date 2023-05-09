f91345306592b44376acf4dceb6bd060fad5cc7e
fodare <folorunsodamilare007@hotmail.com>
2023-05-09 21:52:51 +0200

    Added new home route for mobile view

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -16,30 +16,36 @@ function NavBar() {
                <div className="nav-routes">
                   <nav>
                      <div className="routes">
+                        {/* Home */}
+                        <div className="nav-item-outer home-outer">
+                           <a href="/" className="route-links mobile-only">
+                              <span>Home</span>
+                           </a>
+                        </div>
                         {/* Blogs */}
                         <div className="nav-item-outer">
-                           <a href="/" className="route">
+                           <a href="/blogs" className="route-links blogs">
                               <span>Blogs</span>
                            </a>
                         </div>
 
                         {/* Projects */}
-                        <div className="nav-item-outer" k>
-                           <a href="/" className="">
+                        <div className="nav-item-outer">
+                           <a href="/projects" className="route-links projects">
                               <span>Projects</span>
                            </a>
                         </div>
 
                         {/* Tools */}
-                        <div className="nav-item-outer" k>
-                           <a href="/" className="">
+                        <div className="nav-item-outer">
+                           <a href="/tools" className="route-links tools">
                               <span>Tools</span>
                            </a>
                         </div>
 
                         {/* About */}
-                        <div className="nav-item-outer" k>
-                           <a href="/" className="">
+                        <div className="nav-item-outer">
+                           <a href="/about" className="route-links about">
                               <span>About</span>
                            </a>
                         </div>
@@ -48,9 +54,9 @@ function NavBar() {
                </div>
 
                {/* padding div */}
-               <div>
+               <div className="padding-div">
                   <div className="nav-item-outer ">
-                     <a href="/" className="padding-div">
+                     <a href="/" className="padding-link">
                         <span></span>
                      </a>
                   </div>
