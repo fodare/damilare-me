db868a1a5395e354634e6f640984711c2df1f875
fodare <folorunsodamilare007@hotmail.com>
2023-06-18 10:34:20 +0200

    Correct typo in the about view.

--- src/views/About.jsx
+++ src/views/About.jsx
@@ -11,11 +11,10 @@ function About() {
 
          <div className="aboutMessage">
             <p>
-               <Greetings />, I'm Däre. I'm an IT Ops manager based in Münster
-               Germany. Welcome to my corner of the internet for solutions on
-               technical related how-to's, projects I've created and working on,
-               answers to everyday technical problems, and other things I'd like
-               to share.
+               <Greetings />, I'm Däre. I'm an IT Ops manager. Welcome to my
+               corner of the internet for solutions on technical-related
+               how-to's, projects I've created and worked on, answers to
+               everyday technical problems, and other things I'd like to share.
             </p>
 
             <div id="connect" className="connect-div">
